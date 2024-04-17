801e7a1aece53973e89977450ead96ae38eae268
Swati5490 <101510518+Swati5490@users.noreply.github.com>
2024-04-17 14:56:28 +0530

    Add files via upload

---
+++ ec2/variable.tf
@@ -0,0 +1,24 @@
+variable "ami_id" {
+  
+}
+variable "instance_type" {
+  
+}
+variable "public_key" {
+  
+}
+variable "enable_public_ip_address" {
+  
+}
+variable "tag_name" {
+  
+}
+variable "key_name" {
+  
+}
+variable "user_data_install_jenkins" {
+  
+}
+variable "sg_for_jenkins" {
+  
+}
---
+++ jenkins-runner-script/jenkins-installer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get update
+yes | sudo apt install python3
+echo "Waiting for 30 seconds before installing the java..."
+sleep 30
